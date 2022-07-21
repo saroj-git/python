ca7730af012cef284a25304e9087602b198f9792
herb <autajogi@gmail.com>
2022-07-20 23:57:24 -0700

    understading the variables in python programming

---
+++ variables.py
@@ -0,0 +1,11 @@
+# variables are used to store data values
+# variables in python programming are case sensative 
+# variables name cann't start with number 
+
+num = 5
+country = "Nepal"
+
+# You can know the data type of any given variable by using type() function 
+
+print(type(num)) # This will print out <class 'int'>
+print(type(country))  # This will prit out <class 'str'>
